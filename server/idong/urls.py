3ca89d1dd85a036f67b9ace6718b6f5c4f96a075
bingx86 <sunbingbing@outlook.com>
2013-04-13 23:30:12 +0800

    refactor, add get parties by userid/partyid

--- server/idong/urls.py
+++ server/idong/urls.py
@@ -1,9 +1,13 @@
-
 from django.conf.urls import patterns, include, url
+from rest_framework.urlpatterns import format_suffix_patterns
+from idong import views
 
 urlpatterns = patterns('idong.views',
-    url(r'^users/$',    'user_list'),
-    url(r'^parties/$',  'party_list'),
-    url(r'^parties/(?P<pk>[0-9]+)/$', 'party_detail'),
-    url(r'^userparty/(?P<userid>[0-9]+)/$', 'user_party'),
+    url(r'^users/$',    views.UserList.as_view()),
+    url(r'^parties/$',  views.PartyList.as_view()),
+    url(r'^parties/party=(?P<pk>[0-9]+)/$', views.PartyDetail.as_view()),
+    url(r'^userparty/$', views.UserParty.as_view()),
+    url(r'^userparty/user=(?P<user>[0-9]+)/$', views.UserParty.as_view()),
+    url(r'^userparty/party=(?P<user>[0-9]+)/$', views.UserParty.as_view()),
 )
+urlpatterns = format_suffix_patterns(urlpatterns)
--- server/idong/views.py
+++ server/idong/views.py
@@ -1,80 +1,21 @@
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from rest_framework import generics 
 from idong.models import User, Party, UserParty
 from idong.serializers import UserSerializer, PartySerializer, UserPartySerializer
 
-class JSONResponse(HttpResponse):
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
+
+class UserList(generics.ListCreateAPIView):
+    model = User
+    serializer_class = UserSerializer
+
+class PartyList(generics.ListCreateAPIView):
+    model = Party
+    serializer_class = PartySerializer
+
+class PartyDetail(generics.RetrieveUpdateDestroyAPIView):
+    model = Party
+    serializer_class = PartySerializer
 
 
-@csrf_exempt
-def user_list(request):
-    if request.method == 'GET':
-        users = User.objects.all()
-        serializer = UserSerializer(users)
-        return JSONResponse(serializer.data)
-
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = UserSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JSONResponse(serializer.data, status=201)
-        else:
-            return JSONResponse(serializer.errors, status=400)
-
-
-@csrf_exempt
-def party_list(request):
-    if request.method == 'GET':
-        parties = Party.objects.all()
-        serializer = PartySerializer(parties)
-        return JSONResponse(serializer.data)
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = PartySerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JSONResponse(serializer.data, status=201)
-        else:
-            return JSONResponse(serializer.errors, status=400)
-
-@csrf_exempt
-def party_detail(request, pk):
-    try:
-        party = Party.objects.get(pk=pk)
-    except Party.DoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        serializer = PartySerializer(party)
-        return JSONResponse(serializer.data)
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = PartySerializer(party, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JSONResponse(serializer.data)
-        else:
-            return JSONResponse(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        party.delete()
-        return HttpResponse(status=204)
-
-@csrf_exempt
-def user_party(request, userid):
-    try:
-        parties = UserParty.objects.get(user=userid)
-    except UserParty.DoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        serializer = UserPartySerializer(parties)
-        return JSONResponse(serializer.data)
+class UserParty(generics.ListCreateAPIView):
+    model = UserParty
+    serializer_class = UserPartySerializer
