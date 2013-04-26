ff7fe758a9b9b4dd977273ecb6e023c2a2430ebd
LiJunjie <holysoros@gmail.com>
2013-04-26 21:21:33 +0800

    extract build steps into a single script

---
+++ deploy_to_sae.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# Usually, src_dir should be changed in your situation
+src_dir=server/idong_site
+
+svn_dir=/var/tmp/svn_dir
+
+# remove the deleted items in git from svn repository
+svn_rm_deleted() {
+    deleted_items=`svn status | grep ^! | awk '{ print $2}'`
+    if ! [ -z $deleted_items ]; then
+        svn rm $deleted_items
+    fi
+}
+
+mkdir -p $svn_dir
+echo "Checkout svn repository from SAE"
+svn co https://svn.sinaapp.com/holyweibo/ --username holysoros@163.com --password $SVN_PASSWD --no-auth-cache $svn_dir
+
+echo "Sync from git repository"
+# `1` is the revision of sae application
+rm -rf $svn_dir/1/*
+cp -rf $src_dir/* $svn_dir/1/
+
+cd $svn_dir/1
+svn_rm_deleted
+# Reporting some files are already under version control
+# which can be safely ignored
+svn add * 2>/dev/null
+echo "Deploy to SAE"
+svn ci -m "$TRAVIS_COMMIT" --username holysoros@163.com --password $SVN_PASSWD --no-auth-cache
+echo "Done"
