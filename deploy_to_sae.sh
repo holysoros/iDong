458d181bd969781d157bb23bc15058e083f1f8f1
LiJunjie <holysoros@gmail.com>
2013-05-02 17:07:12 +0800

    update deploy script

--- deploy_to_sae.sh
+++ deploy_to_sae.sh
@@ -1,31 +1,31 @@
-#!/bin/bash
+#!/bin/sh
 set -x
 
 # src_dir is the root directory of all things you want
 # to deploy to SAE svn repository.
 # Usually, it should be changed.
-src_dir=$1
+src_dir="$1"
 
 svn_dir=/var/tmp/svn_dir
 
 # remove the deleted items in git from svn repository
 svn_rm_deleted() {
     deleted_items=`svn status | grep ^! | awk '{ print $2}'`
-    if ! [ -z $deleted_items ]; then
-        svn rm $deleted_items
+    if ! [ -z "$deleted_items" ]; then
+        svn rm "$deleted_items"
     fi
 }
 
 # add items in git from svn repository
 svn_add_new() {
     new_items=`svn status | grep ^? | awk '{ print $2}'`
-    if ! [ -z $new_items ]; then
-        svn add $new_items
+    if ! [ -z "$new_items" ]; then
+        svn add "$new_items"
     fi
 }
 
 echo "Checkout svn repository from SAE"
-svn co https://svn.sinaapp.com/holyweibo/ $svn_dir --username holysoros@163.com --password $SVN_PASSWD --no-auth-cache || exit 1
+svn co https://svn.sinaapp.com/holyweibo/ "$svn_dir" --username holysoros@163.com --password "$SVN_PASSWD" --no-auth-cache || exit 1
 
 echo "Sync from git repository"
 
@@ -40,6 +40,6 @@ svn_rm_deleted
 svn_add_new
 
 echo "Deploy to SAE"
-svn ci -m "$TRAVIS_COMMIT" --username holysoros@163.com --password $SVN_PASSWD --no-auth-cache || exit 1
+svn ci -m "$TRAVIS_COMMIT" --username holysoros@163.com --password "$SVN_PASSWD" --no-auth-cache || exit 1
 
 echo "Done"
