d6e76c438052e2513937753f92f51cc94230ca8e
LiJunjie <holysoros@gmail.com>
2013-05-02 16:20:52 +0800

    update deploy script

--- deploy_to_sae.sh
+++ deploy_to_sae.sh
@@ -1,12 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 set -x
-TRAVIS_COMMIT='commit nothing'
-SVN_PASSWD=jbcb
 
 # src_dir is the root directory of all things you want
 # to deploy to SAE svn repository.
 # Usually, it should be changed.
-src_dir=server/idong_site
+src_dir=$1
 
 svn_dir=/var/tmp/svn_dir
 
@@ -18,26 +16,27 @@ svn_rm_deleted() {
     fi
 }
 
+# add items in git from svn repository
+svn_add_new() {
+    new_items=`svn status | grep ^? | awk '{ print $2}'`
+    if ! [ -z $new_items ]; then
+        svn add $new_items
+    fi
+}
+
 mkdir -p $svn_dir
 echo "Checkout svn repository from SAE"
 svn co https://svn.sinaapp.com/holyweibo/ $svn_dir --username holysoros@163.com --password $SVN_PASSWD --no-auth-cache || exit 1
 
 echo "Sync from git repository"
 
-#cd $src_dir
-#git diff --relative --no-color HEAD^..HEAD >/tmp/diff.patch
-#patch -p1 < /tmp/diff.patch
-
 # `1` is the revision of sae application
-#rm -rf $svn_dir/1/*
+rm -rf $svn_dir/1/*
 cp -rf $src_dir/* $svn_dir/1/ || exit 1
 
 cd $svn_dir/1
-#svn_rm_deleted
-
-# Reporting some files are already under version control
-# which can be safely ignored
-svn add * 2>/dev/null
+svn_rm_deleted
+svn_add_new
 
 echo "Deploy to SAE"
 svn ci -m "$TRAVIS_COMMIT" --username holysoros@163.com --password $SVN_PASSWD --no-auth-cache || exit 1
